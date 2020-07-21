0f7d553ead30890eb4bd90f823fadcdccb5aa931
Shaleen Govil <shaleengovil2000@gmail.com>
2020-07-21 18:11:21 +0530

    Update Lcm_Hcf.cpp

--- Basic/Lcm_Hcf.cpp
+++ Basic/Lcm_Hcf.cpp
@@ -1,5 +1,6 @@
 // To find lcm & hcf of 2 numbers:
 
+// Euclidian algo: recursively
 #include<stdio.h>
 
 int gcd (int a, int b){
@@ -21,3 +22,19 @@ scanf("%d%d",&a,&b);
 printf("LCM:%d\n",lcm(a, b));
 printf("HCF:%d",gcd(a, b));
 }
+
+//Euclidian algo: iteratively
+int main() {
+	// your code goes here
+	int m,n;
+	cin >> m >> n;
+	while(n%m !=0){
+	    int r = n%m;
+	    m = n;
+	    n = r;
+	}
+	cout << n << endl;
+	return 0;
+}
+
+Time Complexity: O(n%m) or O(log(min(n,m)))
